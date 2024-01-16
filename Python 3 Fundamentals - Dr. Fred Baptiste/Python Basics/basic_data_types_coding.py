bf83ebfbfb5d59d598f59876d456cfd0a626ce9a
LA Lucian <la-lucian@protonmail.com>
2024-01-16 12:00:31 +0200

    Basic data types.

---
+++ Python 3 Fundamentals - Dr. Fred Baptiste/Python Basics/basic_data_types_coding.py
@@ -0,0 +1,16 @@
+# Data types examples:
+integer_1 = 1
+integer_2 = 10
+
+float_1 = 1.0
+float_2 = -10.5
+
+boolean_1 = True
+boolean_2 = False
+
+print('Floats are not exact[0.1]:' + format(0.1, '.25f'))
+
+# Comparing numbers
+print('1 + 1 + 1 = 3 is: ' + str(1 + 1 + 1 == 3))
+print('0.125 + 0.125 + 0.125 = 0.375 is: ' + str(0.125 + 0.125 + 0.125 == 0.375))
+print('0.1 + 0.1 + 0.1 = 0.3 is: ' + str(0.1 + 0.1 + 0.1 == 0.3))
